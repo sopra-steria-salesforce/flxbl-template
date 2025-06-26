2bcd4508f4acd1f25eed81640530fe7d347e8b1f
Petter Eikeland <petter.eikeland@soprasteria.com>
2025-06-26 14:26:58 +0200

    Add scripts to .github directory

---
+++ .github/scripts/installPostDependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script installs pre dependencies after all package deployments for every scratch org in the pool
+# (referenced in postDeploymentScriptPath attribute in project-[validate/dev]-pool-def.json)
---
+++ .github/scripts/installPreDependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script installs pre dependencies prior to any package deployment for every scratch org in the pool
+# (referenced in preDeploymentScriptPath attribute in project-[validate/dev]-pool-def.json)
