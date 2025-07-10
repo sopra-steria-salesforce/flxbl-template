b7fc1970c441b0df0c5301ac5d15cbda03d3509e
petter-eikeland <97173615+petter-eikeland@users.noreply.github.com>
2025-07-10 12:36:51 +0200

    Scope profiles to prevent build errors (#18)
    See https://help.salesforce.com/s/articleView?id=release-notes.rn_packaging_scope_profiles.htm&language=en_US&release=248&type=5
    
    * Remove unecessary type attribute

---
+++ .github/scripts/python/scopeProfilesForAllPackages.py
@@ -0,0 +1,10 @@
+import json
+
+with open('sfdx-project.json', 'r+') as f:
+    project = json.load(f)
+    for package in project["packageDirectories"]:
+        package['scopeProfiles'] = True
+    # Write the updated JSON back to the file
+    f.seek(0)
+    json.dump(project, f, indent='\t')
+f.close()
