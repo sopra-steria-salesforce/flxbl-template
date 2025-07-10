ab5c90adaed4dbc5885790f0b899f54bbcd8e4bc
Petter Eikeland <petter.eikeland@soprasteria.com>
2025-07-10 12:45:19 +0200

    Disables Apex Tests for Unchanged packages
    Temp workaround until natively supported on sfp. The feature request is not working: https://github.com/flxbl-io/sfp/issues/5

---
+++ .github/scripts/python/disableApexTestsForUnchangedPackages.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+with open('sfdx-project.json', 'r+') as f:
+    project = json.load(f)
+    for package in project["packageDirectories"]:
+        if os.system('git diff HEAD~1 HEAD --quiet ' + package["path"] + '/*'):
+            print("Changes detected in package "+package["package"]+" --> Apex Tests will be run")
+        else:
+            package["skipTesting"]=True
+
+    # Write the updated JSON back to the file
+    f.seek(0)
+    json.dump(project, f, indent='\t')
+f.close()
