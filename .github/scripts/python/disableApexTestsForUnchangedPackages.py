73ac34c90389187846b368c6a992d6dc984bee87
Petter Eikeland <petter.eikeland@soprasteria.com>
2025-07-11 04:34:08 +0200

    Compare PR changes to main instead of prev commit

--- .github/scripts/python/disableApexTestsForUnchangedPackages.py
+++ .github/scripts/python/disableApexTestsForUnchangedPackages.py
@@ -4,7 +4,7 @@ import os
 with open('sfdx-project.json', 'r+') as f:
     project = json.load(f)
     for package in project["packageDirectories"]:
-        if os.system('git diff HEAD~1 HEAD --quiet ' + package["path"] + '/*'):
+        if os.system('git diff HEAD origin/main --quiet ' + package["path"] + '/*'):
             print("Changes detected in package "+package["package"]+" --> Apex Tests will be run")
         else:
             package["skipTesting"]=True
