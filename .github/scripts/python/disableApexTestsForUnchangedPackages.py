3ce75df340e162c0fdcacb00a360e5b075302fc6
Petter Eikeland <petter.eikeland@soprasteria.com>
2025-07-15 17:00:30 +0200

    Set --no-index flag first

--- .github/scripts/python/disableApexTestsForUnchangedPackages.py
+++ .github/scripts/python/disableApexTestsForUnchangedPackages.py
@@ -4,7 +4,7 @@ import os
 with open('sfdx-project.json', 'r+') as f:
     project = json.load(f)
     for package in project["packageDirectories"]:
-        if os.system('git diff HEAD origin/main --no-index --quiet ' + package["path"] + '/*'):
+        if os.system('git diff --no-index HEAD origin/main  --quiet ' + package["path"] + '/*'):
             print("Changes detected in package "+package["package"]+" --> Apex Tests will be run")
         else:
             package["skipTesting"]=True
