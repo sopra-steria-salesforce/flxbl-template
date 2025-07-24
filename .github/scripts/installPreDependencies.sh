88bbf188695e1fdb062d6f5c17eef36efac3e20e
petter-eikeland <97173615+petter-eikeland@users.noreply.github.com>
2025-07-24 11:24:58 +0200

    Feature/Use native SF Polling instead of static 30 seconds (#25)
    * Disable (local) source tracking for faster deployments
    
    * Move config file
    
    * Use native deployment polling instead of old 30 sec

--- .github/scripts/installPreDependencies.sh
+++ .github/scripts/installPreDependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script installs pre dependencies prior to any package deployment for every scratch org in the pool
 # (referenced in preDeploymentScriptPath attribute in project-[validate/dev]-pool-def.json)
+sf org disable tracking --target-org # Disable source tracking to improve deployment performance
