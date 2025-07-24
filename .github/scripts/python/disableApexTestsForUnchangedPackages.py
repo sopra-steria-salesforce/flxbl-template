02add9d19046361d44c03223ecc787cdbe5631d8
Petter Eikeland <petter.eikeland@soprasteria.com>
2025-07-24 11:45:10 +0200

    Take 2

--- .github/scripts/python/disableApexTestsForUnchangedPackages.py
+++ .github/scripts/python/disableApexTestsForUnchangedPackages.py
@@ -4,7 +4,7 @@ import os
 with open('sfdx-project.json', 'r+') as f:
     project = json.load(f)
     for package in project["packageDirectories"]:
-        if os.system('git diff --quiet HEAD origin/main ' + package["path"] + '/* > /dev/null'):
+        if os.system('git diff --quiet HEAD origin/main ' + package["path"] + '/* > /dev/null 2>&1'):
             print("Changes detected in package "+package["package"]+" --> Apex Tests will be run")
         else:
             package["skipTesting"]=True
