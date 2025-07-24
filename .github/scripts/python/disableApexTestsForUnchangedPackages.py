51822c5e4c610835d95497c674568f0c9ce6517b
Petter Eikeland <petter.eikeland@soprasteria.com>
2025-07-24 11:15:17 +0200

    Remove --no-index flag as it is invalid

--- .github/scripts/python/disableApexTestsForUnchangedPackages.py
+++ .github/scripts/python/disableApexTestsForUnchangedPackages.py
@@ -4,7 +4,7 @@ import os
 with open('sfdx-project.json', 'r+') as f:
     project = json.load(f)
     for package in project["packageDirectories"]:
-        if os.system('git diff --no-index HEAD origin/main  --quiet ' + package["path"] + '/*'):
+        if os.system('git diff HEAD origin/main --quiet ' + package["path"] + '/*'):
             print("Changes detected in package "+package["package"]+" --> Apex Tests will be run")
         else:
             package["skipTesting"]=True
@@ -12,4 +12,4 @@ with open('sfdx-project.json', 'r+') as f:
     # Write the updated JSON back to the file
     f.seek(0)
     json.dump(project, f, indent='\t')
-f.close()
+    f.truncate()
